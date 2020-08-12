37116ac36507da5527c98d6ca70a134d7db5f9e4
Jegyun Kang <tdm1223@naver.com>
2020-08-12 17:38:05 +0900

    [UPDATE] Server
    - 명확한 결과가 나오도록 코드 수정

--- RemoteServer/Server.cpp
+++ RemoteServer/Server.cpp
@@ -1,12 +1,10 @@
-﻿#include <stdio.h>
-#include <string.h>
-#include <winsock2.h>
+﻿#include<stdio.h>
+#include<string.h>
+#include<winsock2.h>
 
 #define BUF_SIZE 100
 #pragma comment(lib,"ws2_32")
 
-void CompressSockets(SOCKET sockArray[], int idx, int total);
-void CompressEvents(WSAEVENT eventArray[], int idx, int total);
 void ErrorHandling(char* msg);
 
 int main()
@@ -15,8 +13,8 @@ int main()
     SOCKET serverSock, clientSock;
     SOCKADDR_IN serverAddress, clientAddress;
 
-    SOCKET socketArray[WSA_MAXIMUM_WAIT_EVENTS];       // 소켓의 핸들 정보를 저장하기 위한 배열
-    WSAEVENT eventArray[WSA_MAXIMUM_WAIT_EVENTS];    // 이벤트 오브젝트 핸들 정보를 저장하기 위한 배열
+    SOCKET socketArray[WSA_MAXIMUM_WAIT_EVENTS]; // 소켓의 핸들 정보를 저장하기 위한 배열
+    WSAEVENT eventArray[WSA_MAXIMUM_WAIT_EVENTS]; // 이벤트 오브젝트 핸들 정보를 저장하기 위한 배열
     WSANETWORKEVENTS netEvents;
 
     int numOfClientSock = 0;
@@ -51,7 +49,7 @@ int main()
     WSAEVENT newEvent = WSACreateEvent(); // 이벤트 오브젝트 생성
 
     // accept 요청, serverSock의 변화발생시 newEvent를 signaled상태로 바꾼다.
-    if (WSAEventSelect(serverSock, newEvent, FD_ACCEPT) == SOCKET_ERROR)
+    if (WSAEventSelect(serverSock, newEvent, FD_ACCEPT | FD_CLOSE) == SOCKET_ERROR)
     {
         ErrorHandling("WSAEventSelect() error");
     }
@@ -78,57 +76,51 @@ int main()
                 signaledEventIdx = i;
                 WSAEnumNetworkEvents(socketArray[signaledEventIdx], eventArray[signaledEventIdx], &netEvents);
 
-                switch (netEvents.lNetworkEvents)
+                if (netEvents.lNetworkEvents & FD_ACCEPT) // 연결 요청시
                 {
-                case FD_ACCEPT:
-                    if (netEvents.lNetworkEvents & FD_ACCEPT) // 연결 요청시
+                    if (netEvents.iErrorCode[FD_ACCEPT_BIT] != 0)
                     {
-                        if (netEvents.iErrorCode[FD_ACCEPT_BIT] != 0)
-                        {
-                            puts("Accept Error");
-                            break;
-                        }
-                        int clientAddressLength = sizeof(clientAddress);
-                        clientSock = accept(socketArray[signaledEventIdx], (SOCKADDR*)&clientAddress, &clientAddressLength);
-                        newEvent = WSACreateEvent();
-                        WSAEventSelect(clientSock, newEvent, FD_READ | FD_CLOSE);
+                        puts("Accept Error");
+                        break;
+                    }
+                    int clientAddressLength = sizeof(clientAddress);
+                    clientSock = accept(socketArray[signaledEventIdx], (SOCKADDR*)&clientAddress, &clientAddressLength);
+                    newEvent = WSACreateEvent();
+                    WSAEventSelect(clientSock, newEvent, FD_READ | FD_CLOSE);
 
-                        eventArray[numOfClientSock] = newEvent;
-                        socketArray[numOfClientSock] = clientSock;
-                        numOfClientSock++;
-                        printf("클라 접속 : IP = %s, Port = %d\n", inet_ntoa(clientAddress.sin_addr), ntohs(clientAddress.sin_port));
-                    }
-                    break;
-                case FD_READ:
-                    if (netEvents.lNetworkEvents & FD_READ) // 데이터 수신
+                    eventArray[numOfClientSock] = newEvent;
+                    socketArray[numOfClientSock] = clientSock;
+                    numOfClientSock++;
+                    printf("클라 접속 : IP = %s, Port = %d\n", inet_ntoa(clientAddress.sin_addr), ntohs(clientAddress.sin_port));
+                }
+
+                if (netEvents.lNetworkEvents & FD_READ) // 데이터 수신
+                {
+                    if (netEvents.iErrorCode[FD_READ_BIT] != 0)
                     {
-                        if (netEvents.iErrorCode[FD_READ_BIT] != 0)
-                        {
-                            puts("Read Error");
-                            break;
-                        }
-                        strLen = recv(socketArray[signaledEventIdx], msg, sizeof(msg), 0);
-                        send(socketArray[signaledEventIdx], msg, strLen, 0);
+                        puts("Read Error");
+                        break;
                     }
-                    break;
-                case FD_CLOSE:
-                    if (netEvents.lNetworkEvents & FD_CLOSE) // 종료 요청
+                    strLen = recv(socketArray[signaledEventIdx], msg, sizeof(msg), 0);
+                    send(socketArray[signaledEventIdx], msg, strLen, 0);
+                }
+
+                if (netEvents.lNetworkEvents & FD_CLOSE) // 종료 요청
+                {
+                    if (netEvents.iErrorCode[FD_CLOSE_BIT] != 0)
                     {
-                        if (netEvents.iErrorCode[FD_CLOSE_BIT] != 0)
-                        {
-                            puts("Close Error");
-                            break;
-                        }
-                        WSACloseEvent(eventArray[signaledEventIdx]);
-                        closesocket(socketArray[signaledEventIdx]);
-                        numOfClientSock--;
-                        for (int i = signaledEventIdx; i < numOfClientSock; i++)
-                        {
-                            socketArray[i] = socketArray[i + 1];
-                            eventArray[i] = eventArray[i + 1];
-                        }
+                        puts("Close Error");
+                        break;
                     }
-                    break;
+                    WSACloseEvent(eventArray[signaledEventIdx]);
+                    closesocket(socketArray[signaledEventIdx]);
+                    printf("종료 된 소켓의 핸들 %d\n", socketArray[signaledEventIdx]);
+                    numOfClientSock--;
+                    for (int i = signaledEventIdx; i < numOfClientSock; i++)
+                    {
+                        socketArray[i] = socketArray[i + 1];
+                        eventArray[i] = eventArray[i + 1];
+                    }
                 }
             }
         }
