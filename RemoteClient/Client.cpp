de8be59746c3be3d6686912c51797617c2710a39
Jegyun Kang <tdm1223@naver.com>
2020-08-12 17:37:03 +0900

    [UPDATE] Client
    - 스레드 사용하도록 수정(연습)

--- RemoteClient/Client.cpp
+++ RemoteClient/Client.cpp
@@ -1,18 +1,19 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <winsock2.h>
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<winsock2.h>
+#include<process.h>
 
 #define BUF_SIZE 1024
 #pragma comment(lib,"ws2_32")
 
 void ErrorHandling(char* message);
+unsigned WINAPI Proc(void *arg); // 메시지 수신 쓰레드 실행 함수
 
 int main()
 {
     WSADATA wsaData;
     SOCKET clientSocket;
-    char message[BUF_SIZE];
     int strLen;
     SOCKADDR_IN serverAddress;
 
@@ -41,19 +42,10 @@ int main()
         printf("서버에 접속 성공 : IP = %s, Port = %d\n", inet_ntoa(serverAddress.sin_addr), ntohs(serverAddress.sin_port));
     }
 
-    while (1)
-    {
-        fputs("Input message(Q to quit): ", stdout);
-        fgets(message, BUF_SIZE, stdin);
-        if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
-        {
-            break;
-        }
-        send(clientSocket, message, strlen(message), 0);
-        strLen = recv(clientSocket, message, BUF_SIZE - 1, 0);
-        message[strLen] = 0;
-        printf("Message from server: %s", message);
-    }
+    DWORD dwThreadID1;
+    HANDLE hThread1 = (HANDLE)_beginthreadex(NULL, 0, Proc, (void*)clientSocket, 0, (unsigned*)&dwThreadID1);
+
+    WaitForSingleObject(hThread1, INFINITE);
 
     closesocket(clientSocket);
     WSACleanup();
@@ -65,4 +57,24 @@ void ErrorHandling(char* message)
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
+}
+
+unsigned WINAPI Proc(void *arg)
+{
+    char message[BUF_SIZE];
+    SOCKET sock = (SOCKET)arg;
+    while (1)
+    {
+        fputs("Input message(Q to quit): ", stdout);
+        fgets(message, BUF_SIZE, stdin);
+        if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+        {
+            closesocket(sock);
+            break;
+        }
+        send(sock, message, strlen(message), 0);
+        int strLen = recv(sock, message, BUF_SIZE - 1, 0);
+        message[strLen] = 0;
+        printf("Message from server: %s", message);
+    }
 }
