99b0b3fecefd108b6df336a29acd71040cb563ec
Jegyun Kang <tdm1223@naver.com>
2020-08-30 14:10:33 +0900

    [UPDATE] Server
    - 큐와 스레드 활용
    - 스레드 여러개 활용은 하지 못하고 있음

--- RemoteServer/Server.h
+++ RemoteServer/Server.h
@@ -1,10 +1,12 @@
-#pragma once
+ï»¿#pragma once
 #include<stdio.h>
 #include<winsock2.h>
 #include<queue>
 #include<thread>
 #include<iostream>
 #include<string>
+#include<condition_variable>  // std::condition_variable
+#include<mutex>
 
 #pragma comment(lib,"ws2_32")
 #pragma warning(disable: 4996)
@@ -14,11 +16,12 @@
 #define MAX_MSG_LEN 256
 #define BUF_SIZE 8096
 
-//ÆÄÀÏ ±âº» Á¤º¸
+//íŒŒì¼ ê¸°ë³¸ ì •ë³´
 struct Files
 {
     char name[MAX_MSG_LEN];
     unsigned int size;
+    int index;
 };
 
 class Server
@@ -32,15 +35,20 @@ public:
     int numOfClient;
 
     void AcceptProc();
-    void ReadProc(int num);
+    void ReadProc(int num, std::queue<Files>* queue, std::mutex* m, std::condition_variable* cv);
     void CloseProc(int num);
+    void RecvProc(std::queue<Files>* queue, std::mutex* m, std::condition_variable* cv);
     void GetClientAddress(SOCKADDR_IN& clientAddress, int index);
+    std::queue<Files> queue;
+
+    std::mutex m;
+    std::condition_variable cv;
 
 private:
-    // ´ë±â ¼ÒÄÏ ¼³Á¤
+    // ëŒ€ê¸° ì†Œì¼“ ì„¤ì •
     SOCKET SetServer(int size);
 
-    // Å¬¶óÀÌ¾ğÆ® ¼ÒÄÏ µî·ÏÇÏ´Â ÇÔ¼ö
+    // í´ë¼ì´ì–¸íŠ¸ ì†Œì¼“ ë“±ë¡í•˜ëŠ” í•¨ìˆ˜
     void AddEvent(SOCKET sock, long eventType);
 
     void EventLoop(SOCKET sock);
