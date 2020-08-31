b33c102781b7e59486c419c0f02c5a717edea417
Jegyun Kang <tdm1223@naver.com>
2020-09-01 00:21:36 +0900

    [UPDATE] Client
    - 클라이언트 스레드, 큐를 사용하여 전달하도록 구현

--- RemoteClient/Main.cpp
+++ RemoteClient/Main.cpp
@@ -5,6 +5,8 @@
 #include<string>
 #include<iostream>
 #include<queue>
+#include<mutex>
+#include<chrono>
 
 #pragma comment(lib, "ws2_32")
 
@@ -14,8 +16,6 @@
 #define BUF_SIZE 4096
 #define MESSAGE_SIZE 20
 
-void SendProc(SOCKET sock);
-
 //파일 기본 정보
 struct Files
 {
@@ -24,13 +24,16 @@ struct Files
     int index = 0;
 };
 
+void SendProc(SOCKET s, std::queue<Files>* q, std::mutex* m);
+
 int main()
 {
+    std::queue<Files> sendQueue;
+    std::mutex m;
     // 윈속 초기화
     WSADATA wsadata;
     if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
     {
-        std::cout << "윈속 초기화 에러" << std::endl;
         return -1;
     }
 
@@ -53,22 +56,11 @@ int main()
         return -1;
     }
 
-    std::thread sendThread(SendProc, sock);
-    sendThread.join();
-
-    WSACleanup(); // 윈속 해제화
-    return 0;
-}
-
-void SendProc(SOCKET s)
-{
-    SOCKET sock = s;
-    FILE* fp;
-    Files sendFile;
     std::string fileName;
-    char endMessage[MESSAGE_SIZE];
-    char buf[BUF_SIZE];
-    char name[256];
+    Files sendFile;
+    FILE* fp;
+
+    std::thread sendThread(SendProc, sock, &sendQueue, &m);
 
     while (true)
     {
@@ -76,166 +68,87 @@ void SendProc(SOCKET s)
         std::cin >> fileName;
         strcpy(sendFile.name, fileName.c_str());
 
-        // 종료라면 소켓 닫기
-        if (strcmp(sendFile.name, "exit") == 0)
-        {
-            closesocket(sock);
-            break;
-        }
-
-        if (strcmp(sendFile.name, "test") == 0)
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                std::string name = std::to_string(i);
-                name += ".txt";
-                std::cout << "name : " << name << std::endl;
-                ZeroMemory(sendFile.name, MAX_MSG_LEN);
-                strcpy(sendFile.name, name.c_str());
-
-                fp = fopen(sendFile.name, "rb");
-                // 파일 끝으로 위치 옮김
-                fseek(fp, 0L, SEEK_END);
-
-                // 파일 크기 얻음
-                sendFile.size = ftell(fp);
-
-                // 다시 파일 처음으로 위치 옮김
-                fseek(fp, 0L, SEEK_SET);
-
-                // 파일 기본 정보 전송
-                std::cout << "전송하는 파일명 : " << sendFile.name << " 전송하는 파일 크기 : " << sendFile.size << " Byte" << std::endl;
-                send(sock, (char*)&sendFile, sizeof(sendFile), 0);
-
-                recv(sock, endMessage, MESSAGE_SIZE, 0);
-                if (strcmp(endMessage, "start") == 0)
-                {
-                    std::cout << "서버로 부터 파일 보내도 된다는 메시지를 받음" << std::endl;
-                }
-
-                // 데이터 통신에 사용할 변수
-                int retval;
-                int numread = 0;
-                int numtotal = 0;
-
-                // 파일 전송
-                while (1)
-                {
-                    numread = fread(buf, 1, BUF_SIZE, fp);
-
-                    if (numread > 0)
-                    {
-                        send(sock, buf, numread, 0);
-                        numtotal += numread;
-                    }
-                    else if (numread == 0 && numtotal == sendFile.size)
-                    {
-                        std::cout << "파일 전송 완료" << std::endl;
-                        break;
-                    }
-                }
-
-                recv(sock, endMessage, MESSAGE_SIZE, 0);
-                if (strcmp(endMessage, "end") == 0)
-                {
-                    std::cout << "파일 전송 완료됨을 받음" << std::endl;
-                }
-                fclose(fp);
-            }
-        }
-
-        if (strcmp(sendFile.name, "test2") == 0)
-        {
-            for (int i = 5; i < 10; i++)
-            {
-                std::string name = std::to_string(i);
-                name += ".txt";
-                std::cout << "name : " << name << std::endl;
-                ZeroMemory(sendFile.name, MAX_MSG_LEN);
-                strcpy(sendFile.name, name.c_str());
-
-                fp = fopen(sendFile.name, "rb");
-                // 파일 끝으로 위치 옮김
-                fseek(fp, 0L, SEEK_END);
-
-                // 파일 크기 얻음
-                sendFile.size = ftell(fp);
-
-                // 다시 파일 처음으로 위치 옮김
-                fseek(fp, 0L, SEEK_SET);
-
-                // 파일 기본 정보 전송
-                std::cout << "전송하는 파일명 : " << sendFile.name << " 전송하는 파일 크기 : " << sendFile.size << " Byte" << std::endl;
-                send(sock, (char*)&sendFile, sizeof(sendFile), 0);
-
-                recv(sock, endMessage, MESSAGE_SIZE, 0);
-                if (strcmp(endMessage, "start") == 0)
-                {
-                    std::cout << "서버로 부터 파일 보내도 된다는 메시지를 받음" << std::endl;
-                }
-
-                // 데이터 통신에 사용할 변수
-                int retval;
-                int numread = 0;
-                int numtotal = 0;
-
-                // 파일 전송
-                while (1)
-                {
-                    numread = fread(buf, 1, BUF_SIZE, fp);
-
-                    if (numread > 0)
-                    {
-                        send(sock, buf, numread, 0);
-                        numtotal += numread;
-                    }
-                    else if (numread == 0 && numtotal == sendFile.size)
-                    {
-                        std::cout << "파일 전송 완료" << std::endl;
-                        break;
-                    }
-                }
-
-                recv(sock, endMessage, MESSAGE_SIZE, 0);
-                if (strcmp(endMessage, "end") == 0)
-                {
-                    std::cout << "파일 전송 완료됨을 받음" << std::endl;
-                }
-                fclose(fp);
-            }
-        }
-
         fp = fopen(sendFile.name, "rb");
         if (fp == NULL)
         {
-            std::cout << "파일이 없습니다. 파일명을 확인하세요" << std::endl;
+            // 종료일 경우
+            if (strcmp(sendFile.name, "exit") == 0)
+            {
+                sendFile.size = 0;
+                sendQueue.push(sendFile);
+                break;
+            }
+            else
+            {
+                std::cout << "파일이 없습니다. 파일명을 확인하세요" << std::endl;
+                continue;
+            }
+        }
+        else
+        {
+            // 파일 끝으로 위치 옮김
+            fseek(fp, 0L, SEEK_END);
+
+            // 파일 크기 얻음
+            sendFile.size = ftell(fp);
+
+            // 다시 파일 처음으로 위치 옮김
+            fseek(fp, 0L, SEEK_SET);
+            fclose(fp);
+
+            std::cout << "큐에 넣는 파일명 : " << sendFile.name << " 전송하는 파일 크기 : " << sendFile.size << " Byte" << std::endl;
+            sendQueue.push(sendFile);
+        }
+    }
+    sendThread.join();
+    closesocket(sock);
+    WSACleanup(); // 윈속 해제화
+    return 0;
+}
+
+void SendProc(SOCKET s, std::queue<Files>* q, std::mutex* m)
+{
+    FILE* fp;
+    SOCKET sock = s;
+    char endMessage[MESSAGE_SIZE];
+    char buf[BUF_SIZE];
+    char name[256];
+
+    while (1)
+    {
+        m->lock();
+        if (q->empty())
+        {
+            m->unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(3000)); // 3초 의도적 지연
             continue;
-        };
+        }
 
-        // 파일 끝으로 위치 옮김
-        fseek(fp, 0L, SEEK_END);
-
-        // 파일 크기 얻음
-        sendFile.size = ftell(fp);
-
-        // 다시 파일 처음으로 위치 옮김
-        fseek(fp, 0L, SEEK_SET);
+        // 큐에서 하나를 꺼냄
+        Files sendFile = q->front();
+        q->pop();
+        if (strcmp(sendFile.name, "exit") == 0 && sendFile.size == 0)
+        {
+            std::cout << "exit" << std::endl;
+            m->unlock();
+            break;
+        }
 
         // 파일 기본 정보 전송
-        std::cout << "전송하는 파일명 : " << sendFile.name << " 전송하는 파일 크기 : " << sendFile.size << " Byte" << std::endl;
         send(sock, (char*)&sendFile, sizeof(sendFile), 0);
 
         // 서버로부터 파일을 보내도 된다는 메시지를 받을때까지 대기
         recv(sock, endMessage, MESSAGE_SIZE, 0);
-        //if (strcmp(endMessage, "start") == 0)
-        //{
-        //    std::cout << "서버로 부터 파일 보내도 된다는 메시지를 받음" << std::endl;
-        //}
+        if (strcmp(endMessage, "start") == 0)
+        {
+            std::cout << "서버로 부터 파일 보내도 된다는 메시지를 받음" << std::endl;
+        }
 
         // 데이터 통신에 사용할 변수
         int retval;
         int numread = 0;
         int numtotal = 0;
+        fp = fopen(sendFile.name, "rb");
 
         // 파일 전송
         while (1)
@@ -260,5 +173,7 @@ void SendProc(SOCKET s)
         {
             std::cout << "파일 전송 완료됨을 받음" << std::endl;
         }
+        m->unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(80));
     }
 }
