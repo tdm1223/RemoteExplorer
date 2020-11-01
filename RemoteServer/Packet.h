0500b410af798bdf059b3d0d4aebaf41e1a0682a
Jegyun Kang <tdm1223@naver.com>
2020-11-01 12:04:46 +0900

    [UPDATE] Packet
    - 불필요한 주석 제거

--- RemoteServer/Packet.h
+++ RemoteServer/Packet.h
@@ -10,9 +10,9 @@ public:
 
     void serialize(std::vector<char>& buf);
 
-    char prefix = 0x7F; // Marks the begin of `Packet`
-    char command = '0'; // Identifies the packet's type - what is it used for.
-    int size = 0; // Size of `data`.
-    std::vector<char> data; // Custom data.
+    char prefix = 0x7F;
+    char command = '0';
+    int size = 0;
+    std::vector<char> data;
     char suffix = 0x7F;
 };
