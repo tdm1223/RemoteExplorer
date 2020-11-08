57218136e4fa4f7c66093d9add41b9e7d8cf48ec
Jegyun Kang <tdm1223@naver.com>
2020-11-08 21:31:53 +0900

    [UPDATE] Client
    - thread를 사용하여 전송하도록 수정

--- RemoteClient/Main.cpp
+++ RemoteClient/Main.cpp
@@ -4,6 +4,7 @@
 #include"Packet.h"
 #include<filesystem>
 #include"Parser.h"
+#include <thread>
 
 namespace fs = std::filesystem;
 
@@ -16,11 +17,10 @@ namespace fs = std::filesystem;
 #define DOWNLOAD 2
 #define END 3
 
+void FileUpload(SOCKET s);
+
 int main()
 {
-    // receive 버퍼
-    char buffer[BUF_SIZE];
-
     // client 소켓 선언
     SOCKET clientSock;
 
@@ -67,103 +67,10 @@ int main()
         std::cout << "3 - END" << std::endl;
         std::cin >> packet.command;
 
-        // 보낼 데이터 버퍼
-        char data[BUF_SIZE - sizeof(char) - 2 * sizeof(int)];
-
         if (packet.command == UPLOAD)
         {
-            // 현재 폴더에 있는 파일 출력
-            std::cout << "현재 폴더에 있는 파일" << std::endl;
-            std::cout << "======================" << std::endl;
-            std::string files;
-            for (const fs::directory_entry& entry : fs::directory_iterator(fs::current_path()))
-            {
-                files = entry.path().string();
-                size_t pos = files.rfind("\\");
-                std::cout << files.substr(pos + 1) << std::endl;
-            }
-            std::cout << "======================" << std::endl;
-
-            // 파일명 입력 받음
-            std::string fileName = "";
-            std::cout << "파일명 : ";
-            std::cin >> fileName;
-
-            FILE* fp;
-            fp = fopen(fileName.c_str(), "rb");
-            if (fp == NULL)
-            {
-                std::cout << "없는 파일입니다." << std::endl;
-                continue;
-            }
-            else
-            {
-                // 파일명을 먼저 전달
-                packet.size = fileName.length();
-                strcpy(data, fileName.c_str());
-
-                // 패킷 빌드
-                int offset = 0;
-                packet.Build(buffer, UPLOAD, fileName.length(), data, offset);
-
-                // 메세지 전송
-                std::cout << "전송하는 메시지 크기 : " << offset << std::endl;
-                if (send(clientSock, buffer, offset, 0) < 0)
-                {
-                    return 0;
-                }
-
-                // 메시지 전송 후 수신을 기다림
-                if (packet.command == UPLOAD)
-                {
-                    while (true)
-                    {
-                        memset(buffer, 0, sizeof(buffer));
-
-                        // 파일 끝으로 위치 옮김
-                        fseek(fp, 0L, SEEK_END);
-
-                        // 파일 크기 얻음
-                        packet.size = ftell(fp);
-
-                        // 다시 파일 처음으로 위치 옮김
-                        fseek(fp, 0L, SEEK_SET);
-                        fclose(fp);
-                        std::cout << "큐에 넣는 파일명 : " << fileName << " 전송하는 파일 크기 : " << packet.size << " Byte" << std::endl;
-
-                        offset = 0;
-                        packet.buf.clear();
-                        int size = sprintf(data, "%d", packet.size);
-                        //std::cout << "size : "<<size << std::endl;
-                        packet.Build(buffer, UPLOAD, size, data, offset);
-                        send(clientSock, buffer, offset, 0);
-
-                        // 데이터 통신에 사용할 변수
-                        int sendSize = 0;
-                        fp = fopen(fileName.c_str(), "rb");
-
-                        int totalSize = 0;
-                        // 파일 전송
-                        while (1)
-                        {
-                            Packet filePacket;
-                            offset = 0;
-                            sendSize = fread(data, 1, BUF_SIZE - sizeof(char) - 2 * sizeof(int), fp);
-                            filePacket.Build(buffer, UPLOAD, sendSize, data, offset);
-                            totalSize += sendSize;
-                            //std::cout << "sendSize : " << sendSize << " totalSize : " << totalSize << std::endl;
-                            if (sendSize < BUF_SIZE - filePacket.GetHeaderSize())
-                            {
-                                send(clientSock, buffer, offset, 0);
-                                break;
-                            }
-                            send(clientSock, buffer, BUF_SIZE, 0);
-                        }
-                        fclose(fp);
-                        break;
-                    }
-                }
-            }
+            std::thread uploadThread([=] {FileUpload(clientSock); });
+            uploadThread.join();
         }
         else if (packet.command == DOWNLOAD)
         {
@@ -181,6 +88,108 @@ int main()
     return 0;
 }
 
+void FileUpload(SOCKET s)
+{
+    Packet packet;
+    packet.command = UPLOAD;
+    SOCKET sock = s;
+
+    // receive 버퍼
+    char buffer[BUF_SIZE];
+
+    // 보낼 데이터 버퍼
+    char data[BUF_SIZE - sizeof(char) - 2 * sizeof(int)];
+
+    // 현재 폴더에 있는 파일 출력
+    std::cout << "현재 폴더에 있는 파일" << std::endl;
+    std::cout << "======================" << std::endl;
+    std::string files;
+    for (const fs::directory_entry& entry : fs::directory_iterator(fs::current_path()))
+    {
+        files = entry.path().string();
+        size_t pos = files.rfind("\\");
+        std::cout << files.substr(pos + 1) << std::endl;
+    }
+    std::cout << "======================" << std::endl;
+
+    // 파일명 입력 받음
+    std::string fileName = "";
+    std::cout << "파일명 : ";
+    std::cin >> fileName;
+
+    FILE* fp;
+    fp = fopen(fileName.c_str(), "rb");
+    if (fp == NULL)
+    {
+        std::cout << "없는 파일입니다." << std::endl;
+        return;
+    }
+    else
+    {
+        // 파일명을 먼저 전달
+        packet.size = fileName.length();
+        strcpy(data, fileName.c_str());
+
+        // 패킷 빌드
+        int offset = 0;
+        packet.Build(buffer, UPLOAD, fileName.length(), data, offset);
+
+        // 메세지 전송
+        std::cout << "전송하는 메시지 크기 : " << offset << std::endl;
+        if (send(sock, buffer, offset, 0) < 0)
+        {
+            return;
+        }
+
+        while (true)
+        {
+            memset(buffer, 0, sizeof(buffer));
+
+            // 파일 끝으로 위치 옮김
+            fseek(fp, 0L, SEEK_END);
+
+            // 파일 크기 얻음
+            packet.size = ftell(fp);
+
+            // 다시 파일 처음으로 위치 옮김
+            fseek(fp, 0L, SEEK_SET);
+            fclose(fp);
+            std::cout << "전송하는 파일명 : " << fileName << " 전송하는 파일 크기 : " << packet.size << " Byte" << std::endl;
+
+            offset = 0;
+            packet.buf.clear();
+            int size = sprintf(data, "%d", packet.size);
+            //std::cout << "size : "<<size << std::endl;
+            packet.Build(buffer, UPLOAD, size, data, offset);
+            send(sock, buffer, offset, 0);
+
+            // 데이터 통신에 사용할 변수
+            int sendSize = 0;
+            fp = fopen(fileName.c_str(), "rb");
+
+            int totalSize = 0;
+            // 파일 전송
+            while (1)
+            {
+                Packet filePacket;
+                offset = 0;
+                sendSize = fread(data, 1, BUF_SIZE - sizeof(char) - 2 * sizeof(int), fp);
+                filePacket.Build(buffer, UPLOAD, sendSize, data, offset);
+                totalSize += sendSize;
+                //std::cout << "sendSize : " << sendSize << " totalSize : " << totalSize << std::endl;
+                if (sendSize < BUF_SIZE - filePacket.GetHeaderSize())
+                {
+                    send(sock, buffer, offset, 0);
+                    break;
+                }
+                send(sock, buffer, BUF_SIZE, 0);
+            }
+            fclose(fp);
+            break;
+        }
+    }
+    return;
+}
 //void RecvProc(SOCKET s)
 //{
 //    SOCKET sock = s;
