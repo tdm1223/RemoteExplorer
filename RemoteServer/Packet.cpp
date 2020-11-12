69951eec29f2276969fb0a68ed6486c0b87f749d
Jegyun Kang <tdm1223@naver.com>
2020-11-12 19:01:32 +0900

    [UPDATE] Packet
    - 임시 코드 수정

--- RemoteServer/Packet.cpp
+++ RemoteServer/Packet.cpp
@@ -1,71 +1,71 @@
-#include "Packet.h"
-
-Packet::Packet()
-{
-    command = 0;
-    size = 0;
-}
-
-Packet::~Packet()
-{}
-
-void Packet::WriteWithoutData(const int command, const int size, int& offset)
-{
-    int dataSize = sizeof(command) + sizeof(size);
-
-    buf.resize(dataSize);
-
-    memcpy(&buf[offset], command, sizeof(command));
-    offset += sizeof(command);
-    
-    memcpy(&buf[offset], size, sizeof(size));
-    offset += sizeof(size);
-
-
-    buf.push_back(prefix);
-    buf.push_back(command & 0xFF);
-    buf.push_back((command >> 8) & 0xFF);
-    buf.push_back((command >> 16) & 0xFF);
-    buf.push_back((command >> 24) & 0xFF);
-
-    buf.push_back(size & 0xFF);
-    buf.push_back((size >> 8) & 0xFF);
-    buf.push_back((size >> 16) & 0xFF);
-    buf.push_back((size >> 24) & 0xFF);
-
-    offset += sizeof(char) + sizeof(int) * 2;
-}
-
-void Packet::WriteData(char* data, int size, int& offset)
-{
-    offset += size;
-    for (int i = 0; i < size; i++)
-    {
-        buf.push_back(data[i]);
-    }
-}
-
-void Packet::Serialize(char* data)
-{
-    memcpy(data, &buf[0], buf.size());
-}
-
-/// <summary>
-/// 패킷 생성하는 함수
-/// </summary>
-/// <param name="buffer">패킷 버퍼</param>
-/// <param name="data">데이터</param>
-/// <param name="command">명령 종류</param>
-/// <param name="size">데이터 길이</param>
-/// <param name="offset">패킷의 총 길이</param>
-void Packet::Build(char* buffer, int command, int size, char* data, int& offset)
-{
-    WriteWithoutData(command, size, offset);
-    WriteData(data, size, offset);
-    Serialize(buffer);
-}
-
-int Packet::GetHeaderSize()
-{
-    return 2 * sizeof(int) + sizeof(char);
-}
+//#include "Packet.h"
+//
+//Packet::Packet()
+//{
+//    command = 0;
+//    size = 0;
+//}
+//
+//Packet::~Packet()
+//{}
+//
+//void Packet::WriteWithoutData(const int command, const int size, int& offset)
+//{
+//    int dataSize = sizeof(command) + sizeof(size);
+//
+//    buf.resize(dataSize);
+//
+//    memcpy(&buf[offset], command, sizeof(command));
+//    offset += sizeof(command);
+//    
+//    memcpy(&buf[offset], size, sizeof(size));
+//    offset += sizeof(size);
+//
+//
+//    buf.push_back(prefix);
+//    buf.push_back(command & 0xFF);
+//    buf.push_back((command >> 8) & 0xFF);
+//    buf.push_back((command >> 16) & 0xFF);
+//    buf.push_back((command >> 24) & 0xFF);
+//
+//    buf.push_back(size & 0xFF);
+//    buf.push_back((size >> 8) & 0xFF);
+//    buf.push_back((size >> 16) & 0xFF);
+//    buf.push_back((size >> 24) & 0xFF);
+//
+//    offset += sizeof(char) + sizeof(int) * 2;
+//}
+//
+//void Packet::WriteData(char* data, int size, int& offset)
+//{
+//    offset += size;
+//    for (int i = 0; i < size; i++)
+//    {
+//        buf.push_back(data[i]);
+//    }
+//}
+//
+//void Packet::Serialize(char* data)
+//{
+//    memcpy(data, &buf[0], buf.size());
+//}
+//
+///// <summary>
+///// 패킷 생성하는 함수
+///// </summary>
+///// <param name="buffer">패킷 버퍼</param>
+///// <param name="data">데이터</param>
+///// <param name="command">명령 종류</param>
+///// <param name="size">데이터 길이</param>
+///// <param name="offset">패킷의 총 길이</param>
+//void Packet::Build(char* buffer, int command, int size, char* data, int& offset)
+//{
+//    WriteWithoutData(command, size, offset);
+//    WriteData(data, size, offset);
+//    Serialize(buffer);
+//}
+//
+//int Packet::GetHeaderSize()
+//{
+//    return 2 * sizeof(int) + sizeof(char);
+//}
--- RemoteServer/Packet.h
+++ RemoteServer/Packet.h
@@ -3,7 +3,7 @@
 #include<winsock.h>
 #include<iostream>
 
-class Pakcet
+class Packet
 {
 public:
     virtual const bool OnParse(const char* buffer, const unsigned int bufferSize) = 0;
@@ -13,85 +13,101 @@ public:
 class MyPacket : public Packet
 {
 public:
+    void SetCommand(int command)
+    {
+        this->command = command;
+    }
+
+    void SetSize(int size)
+    {
+        this->size = size;
+    }
+
     const int GetCommand() const
     {
         return command;
     }
+    
+    const int GetSize() const
+    {
+        return size;
+    }
 
     virtual const bool OnParse(const char* buffer, const unsigned int bufferSize) override
     {
-        if (buffer == nullptr || bufferSize < header + body)
-        {
-            return false;
-        }
+       // if (buffer == nullptr || bufferSize < header + body)
+       // {
+       //     return false;
+       // }
 
-       //...
-        command = 1;
+       ////...
+       // command = 1;
 
-        //...
-        size = 2;
+       // //...
+       // size = 2;
 
         return true;
     }
 
     virtual const bool OnBuild(char* buffer, unsigned int& buildBufferSize) override
     {
-        if (command > command validate || size > size validate)
+        if (command > END || size < 0)
         {
-            return false
+            return false;
         }
 
-        memcpy(buffer, command, 4);
-        memcpy(buffer + 4, size, 4);
+        memcpy(buffer, &command, 4);
+        buildBufferSize += 4;
 
-
-        buildbufferSize = 8;
+        memcpy(buffer + 4, &size, 4);
+        buildBufferSize += 4;
 
         return true;
     }
 
 private:
+    std::string prefix;
     int command;
     int size;
 };
 
-server code
-{
-    //case Send
-    MyPacket packet;
-   packet.command = 4;
-    packet.size = 8;
-
-    char pakcetBuffer[MAX_PACKET] = { 0, };
-    uint32_t buildBufferSize = 0;
-    if (packet.OnBuild(packetBuffer, buildBufferSize) == true)
-    {
-        Send(socket, packetBuffer, buildBufferSize);
-    }
-
-    //case Recv
-    MyPacket packet;
-    if (packet.OnParse(recvBuffer, 8) == true)
-    {
-        packet.GetCommand();
-    }
-
-}
-
-class Packet
-{
-public:
-    Packet();
-    ~Packet();
-
-    void WriteWithoutData(int command, int size, int& offset);
-    void WriteData(char* data, int size, int& offset);
-    void Serialize(char* data);
-    void Build(char* buffer, int command, int size, char* data, int& offset);
-    int GetHeaderSize();
-    char prefix = 0x7F;
-    int command;
-    int size;
-
-    std::vector<char> buf;
-};
+//server code
+//{
+//    //case Send
+//    MyPacket packet;
+//   packet.command = 4;
+//    packet.size = 8;
+//
+//    char pakcetBuffer[MAX_PACKET] = { 0, };
+//    uint32_t buildBufferSize = 0;
+//    if (packet.OnBuild(packetBuffer, buildBufferSize) == true)
+//    {
+//        Send(socket, packetBuffer, buildBufferSize);
+//    }
+//
+//    //case Recv
+//    MyPacket packet;
+//    if (packet.OnParse(recvBuffer, 8) == true)
+//    {
+//        packet.GetCommand();
+//    }
+//
+//}
+//
+//class Packet
+//{
+//public:
+//    Packet();
+//    ~Packet();
+//
+//    void WriteWithoutData(int command, int size, int& offset);
+//    void WriteData(char* data, int size, int& offset);
+//    void Serialize(char* data);
+//    void Build(char* buffer, int command, int size, char* data, int& offset);
+//    int GetHeaderSize();
+//    char prefix = 0x7F;
+//    int command;
+//    int size;
+//
+//    std::vector<char> buf;
+//};
