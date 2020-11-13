b204a1145af4b924d8aeb1d33d6e5764cfbee893
Jegyun Kang <tdm1223@naver.com>
2020-11-13 18:03:25 +0900

    [UPDATE] Server, Client
    - 가이드대로 수정
    - 파일전송은 아직 수정하지 않음

--- RemoteServer/Packet.h
+++ RemoteServer/Packet.h
@@ -8,12 +8,15 @@ class Packet
 public:
     virtual const bool OnParse(const char* buffer, const unsigned int bufferSize) = 0;
     virtual const bool OnBuild(char* buffer, unsigned int& buildBufferSize) = 0;
+    virtual const void Clear() = 0;
+
+    const std::string kPrefix = "ESTSOFT";
 };
 
 class MyPacket : public Packet
 {
 public:
-    enum {UPLOAD = 1, DOWNLOAD = 2, END = 3};
+    enum { UPLOAD = 1, DOWNLOAD = 2, END = 3 };
     void SetCommand(int command)
     {
         this->command = command;
@@ -24,52 +27,107 @@ public:
         this->size = size;
     }
 
+    void SetPrefix(const char* prefix)
+    {
+        strcpy(this->prefix, prefix);
+    }
+
+    void SetData(std::vector<char> data)
+    {
+        this->data = data;
+    }
+
     const int GetCommand() const
     {
         return command;
     }
-    
+
     const int GetSize() const
     {
         return size;
     }
 
+    const char* GetPrefix() const
+    {
+        return prefix;
+    }
+
+    const std::vector<char> GetData() const
+    {
+        return data;
+    }
+
     virtual const bool OnParse(const char* buffer, const unsigned int bufferSize) override
     {
-       // if (buffer == nullptr || bufferSize < header + body)
-       // {
-       //     return false;
-       // }
+        if (buffer == nullptr) //  || bufferSize < header + body
+        {
+            return false;
+        }
 
-       ////...
-       // command = 1;
+        int offset = 0;
 
-       // //...
-       // size = 2;
+        memcpy(&prefix[0], buffer, 8);
+        offset += 8;
+
+        if (strcmp(prefix, kPrefix.c_str()) != 0)
+        {
+            std::cout << "´Ù¸£´Ù..." << std::endl;
+            return false;
+        }
+
+        memcpy(&command, buffer + offset, 4);
+        offset += 4;
+
+        memcpy(&size, buffer + offset, 4);
+        offset += 4;
+
+        data.resize(size);
+        memcpy(&data[0], buffer + offset, size);
+        offset += size;
+
+        if (offset != bufferSize)
+        {
+            return false;
+        }
 
         return true;
     }
 
     virtual const bool OnBuild(char* buffer, unsigned int& buildBufferSize) override
     {
-        if (command > END || size < 0)
+        if ((command > END || command < UPLOAD) || size < 0)
         {
             return false;
         }
 
-        memcpy(buffer, &command, 4);
+        memcpy(buffer, &prefix, sizeof(char) * 8);
+        buildBufferSize += 8;
+
+        memcpy(buffer + buildBufferSize, &command, 4);
         buildBufferSize += 4;
 
-        memcpy(buffer + 4, &size, 4);
+        memcpy(buffer + buildBufferSize, &size, 4);
         buildBufferSize += 4;
+
+        memcpy(buffer + buildBufferSize, &data[0], size);
+        buildBufferSize += size;
 
         return true;
     }
 
+    virtual const void Clear() override
+    {
+        memset(prefix, 0, 8);
+        command = -1;
+        size = -1;
+        data.clear();
+    }
+
 private:
-    std::string prefix;
+    char prefix[8];
     int command;
     int size;
+    std::vector<char> data;
 };
 
 //server code
