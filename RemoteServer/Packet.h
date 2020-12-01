b70753fbe8e5fca3d3c7d0ab0b7925b49d691570
Jegyun Kang <tdm1223@naver.com>
2020-12-01 21:21:41 +0900

    [UPDATE] Packet
    - 복사를 방지하는 클래스를 추가하고 packet 클래스가 이를 상속받도록 수정

--- RemoteServer/Packet.h
+++ RemoteServer/Packet.h
@@ -3,7 +3,19 @@
 #include<winsock.h>
 #include<iostream>
 
-class Packet
+class Uncopyable 
+{
+protected:
+    // packet 클래스의 생성과 소멸은 허용
+    Uncopyable() {};
+    ~Uncopyable() {};
+private:
+    // packet 클래스의 객체 복사 방지
+    Uncopyable(const Uncopyable&);
+    Uncopyable& operator=(const Uncopyable&) {};
+};
+
+class Packet : private Uncopyable
 {
 public:
     virtual const bool OnParse(const char* buffer, const unsigned int bufferSize) = 0;
