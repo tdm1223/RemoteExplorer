de0fe71e04d7e47bdbcd64f7b36ad2905009b7f8
Jegyun Kang <tdm1223@naver.com>
2021-03-01 15:27:03 +0900

    클라이언트 소켓 클래스 추가

---
+++ RemoteClient/ClientSocket.cpp
@@ -0,0 +1,86 @@
+﻿#include"ClientSocket.h"
+#include"UploadCommand.h"
+#include"DownloadCommand.h"
+#include"MoveCommand.h"
+#include"Receiver.h"
+#include<thread>
+
+ClientSocket* ClientSocket::clientSocket = nullptr;
+
+ClientSocket::ClientSocket()
+{
+    AddPacketCommand(nullptr);
+    AddPacketCommand(new UploadCommand);
+    AddPacketCommand(new DownloadCommand);
+    AddPacketCommand(new MoveCommand);
+}
+
+void ClientSocket::AddPacketCommand(PacketCommand* packetCommand)
+{
+    packetCommands.push_back(std::unique_ptr<PacketCommand>(packetCommand));
+}
+
+ClientSocket::~ClientSocket()
+{
+}
+
+ClientSocket* ClientSocket::GetInstance()
+{
+    if (clientSocket == nullptr)
+    {
+        clientSocket = new ClientSocket();
+        atexit(ClientSocket::releaseInstance);
+    }
+    return clientSocket;
+}
+
+void ClientSocket::releaseInstance()
+{
+    if (clientSocket)
+    {
+        closesocket(clientSocket->sock);
+        clientSocket->sock = INVALID_SOCKET;
+        delete clientSocket;
+        clientSocket = nullptr;
+    }
+}
+
+SOCKET& ClientSocket::GetSocket()
+{
+    return sock;
+}
+
+void ClientSocket::CloseSocket()
+{
+    closesocket(sock);
+    std::cout << "연결 종료" << std::endl;
+}
+
+void ClientSocket::Connect(int port)
+{
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        exit(1);
+    }
+
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    memset(&serverAddress, 0, sizeof(serverAddress));
+
+    // 서버 주소 지정
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddress.sin_port = htons(port);
+
+    // 서버에 접속 요청 
+    if (connect(sock, (LPSOCKADDR)&serverAddress, sizeof(serverAddress)) != 0)
+    {
+        std::cout << GetLastError() << std::endl;
+        exit(1);
+    }
+    std::cout << "서버와 연결 성공" << std::endl;
+
+    Receiver receiver;
+    std::thread commandThread(receiver, &packetCommands);
+    commandThread.join();
+}
---
+++ RemoteClient/ClientSocket.h
@@ -0,0 +1,36 @@
+﻿#ifndef CLIENT_SOCKET_H
+#define CLIENT_SOCKET_H
+
+#include<WinSock2.h>
+#include<vector>
+#include<memory>
+#include"PacketCommand.h"
+
+class Client;
+
+class ClientSocket
+{
+public:
+    virtual ~ClientSocket();
+    static ClientSocket* GetInstance();
+    static void releaseInstance();
+    SOCKET& GetSocket();
+    void CloseSocket();
+    void Connect(int port);
+
+private:
+    ClientSocket();
+    void AddPacketCommand(PacketCommand* packetCommand);
+    static ClientSocket* clientSocket;
+    SOCKET sock;
+    WSADATA wsaData;
+    SOCKADDR_IN serverAddress;
+    bool cleanSocket;
+
+    DWORD flags;
+    DWORD recvBytes;
+
+    std::vector<std::unique_ptr<PacketCommand>> packetCommands;
+};
+
+#endif
