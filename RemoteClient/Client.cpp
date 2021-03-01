74a9bb2586cb2d8b22602f7c36e0a6105e90c06f
Jegyun Kang <tdm1223@naver.com>
2021-03-01 15:27:22 +0900

    클라이언트ㅇ서 클라이언트 소켓 사용하도록 수정

--- RemoteClient/Client.cpp
+++ RemoteClient/Client.cpp
@@ -1,57 +1,15 @@
-﻿#include "Client.h"
+﻿#include"Client.h"
 #include<thread>
-#include"UploadCommand.h"
-#include"DownloadCommand.h"
-#include"MoveCommand.h"
 
 Client::Client()
 {
-    AddPacketCommand(nullptr);
-    AddPacketCommand(new UploadCommand);
-    AddPacketCommand(new DownloadCommand);
-    AddPacketCommand(new MoveCommand);
-    // 소켓을 초기화
-    WSADATA wsadata;
-    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
-    {
-        exit(1);
-    }
-
-    // client 소켓 생성
-    clientSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-    if (clientSock == INVALID_SOCKET)
-    {
-        WSACleanup();
-        exit(1);
-    }
-
-    memset(&serverAddress, 0, sizeof(serverAddress));
-
-    // 서버 주소 지정
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serverAddress.sin_port = htons(PORT);
-
-    // 서버에 접속 요청 
-    if (connect(clientSock, (LPSOCKADDR)&serverAddress, sizeof(serverAddress)) != 0)
-    {
-        exit(1);
-    }
+    clientSock = ClientSocket::GetInstance();
+    clientSock->Connect(kPort);
 }
 
+#include<iostream>
 void Client::Initialize()
 {
-    Receiver receiver;
-    std::thread commandThread(receiver, &commands);
-    commandThread.join();
-
     // 소켓을 닫음
-    closesocket(clientSock);
-    std::cout << "client exit" << std::endl;
-}
-
-void Client::AddPacketCommand(PacketCommand* command)
-{
-    commands.push_back(std::unique_ptr<PacketCommand>(command));
+    clientSock->CloseSocket();
 }
