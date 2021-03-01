be58665abdb72f9f3ad7930008863ca8689476a7
Jegyun Kang <tdm1223@naver.com>
2021-03-01 15:27:22 +0900

    클라이언트ㅇ서 클라이언트 소켓 사용하도록 수정

--- RemoteClient/Client.h
+++ RemoteClient/Client.h
@@ -1,11 +1,6 @@
 ﻿#pragma once
 #include<WinSock2.h>
-#include<string>
-#include<filesystem>
-#include"Receiver.h"
-#include"PacketCommand.h";
-
-namespace fs = std::filesystem;
+#include"ClientSocket.h"
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -15,14 +10,7 @@ public:
     Client();
     void Initialize();
 private:
-    const int PORT = 9000;
-    // client 소켓 선언
-    SOCKET clientSock;
-
-    // 서버주소 구조체 변수 선언
-    SOCKADDR_IN serverAddress;
-
+    const int kPort = 9000;
     std::vector<std::unique_ptr<PacketCommand>> commands;
-
-    void AddPacketCommand(PacketCommand* command);
+    ClientSocket* clientSock;
 };
