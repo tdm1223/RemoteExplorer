ce7602237e3c80e802a7b2aed9c24755d98db722
Jegyun Kang <tdm1223@naver.com>
2021-04-04 00:38:03 +0900

    [CREATE] Server, Client의 DOWNLOAD 커맨드 구현

--- RemoteClient/DownloadCommand.cpp
+++ RemoteClient/DownloadCommand.cpp
@@ -20,5 +20,26 @@ bool DownloadCommand::Execute(SOCKET sock, void* buffer)
     int length = 0;
     RecvLength(sock, &length);
     std::cout << "LENGTH : " << length << std::endl;
+
+    FILE* fp;
+    if (length > 0)
+    {
+        while (1)
+        {
+            fp = fopen(fileName.c_str(), "wb+");
+            RecvLength(sock, &length);
+            std::cout << "length : " << length << std::endl;
+            memset(buffers, 0, Util::kBufferSize);
+            if (length < Util::kBufferSize)
+            {
+                recv(sock, buffers, length, 0);
+                fwrite(buffers, 1, length, fp);
+                break;
+            }
+            recv(sock, buffers, length, 0);
+            fwrite(buffers, 1, length, fp);
+        }
+        fclose(fp);
+    }
     return true;
 }
--- RemoteServer/DownloadCommand.cpp
+++ RemoteServer/DownloadCommand.cpp
@@ -15,21 +15,40 @@ bool DownloadCommand::Execute(SOCKET sock, char* buf)
     if (fp == NULL)
     {
         std::cout << "없는 파일" << std::endl;
+        return false;
     }
-    else
-    {
-        std::cout << "있는 파일" << std::endl;
-        fseek(fp, 0, SEEK_END);
-        fileSize = ftell(fp);
-    }
+    std::cout << "있는 파일" << std::endl;
+    fseek(fp, 0, SEEK_END);
+    fileSize = ftell(fp);
 
-    fclose(fp);
     char buffer[Util::kLengthSize];
     SerializeInt(fileSize, buffer);
     send(sock, buffer, Util::kLengthSize, 0);
-
+    rewind(fp);
+    int sendSize = 0;
     // 파일 크기가 0보다 크다면 파일 전송
+    if (fileSize > 0)
+    {
+        while (fileSize > 0)
+        {
+            char dataBuffer[Util::kBufferSize];
+            memset(dataBuffer, 0, Util::kBufferSize);
+            sendSize = fread(dataBuffer, 1, fileSize, fp);
+            std::cout << "sendsize  : " << sendSize << std::endl;
+            SerializeInt(sendSize, buffer);
+            send(sock, buffer, Util::kLengthSize, 0);
+            if (sendSize < Util::kBufferSize)
+            {
+                std::cout << sendSize << "보냄" << std::endl;
+                send(sock, dataBuffer, sendSize, 0);
+                break;
+            }
+            send(sock, dataBuffer, Util::kBufferSize, 0);
+            fileSize -= sendSize;
+        }
+    }
 
+    fclose(fp);
     // 0이면 파일이 없는것
     return true;
 }
