20c312b0e1f9cd3087451af7080287b5191c4943
Jegyun Kang <tdm1223@naver.com>
2021-04-21 02:04:10 +0900

    [CREATE] win32 프로그램으로 만들 UIClient 프로젝트 추가

---
+++ UIClient/UIClient.cpp
@@ -0,0 +1,68 @@
+﻿// UIClient.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "UIClient.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    WNDCLASS wc;
+
+    wchar_t my_class_name[] = L"RemoteClient";
+    wc.cbClsExtra = NULL;
+    wc.cbWndExtra = NULL;
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hInstance = hInstance;
+    wc.lpfnWndProc = WndProc;
+    wc.lpszClassName = my_class_name;
+    wc.lpszMenuName = NULL;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+
+    RegisterClass(&wc);
+
+    HWND hWnd = CreateWindow(my_class_name, L"RemoteClient", WS_OVERLAPPEDWINDOW, 100, 100, 1200, 800, NULL, NULL, hInstance, NULL);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    if (uMsg == WM_DESTROY) PostQuitMessage(0);
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
